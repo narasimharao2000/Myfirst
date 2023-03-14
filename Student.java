7375735174cbdee32397b81b6dbabaa0ed646ffa
narasimharao2000 <127827834+narasimharao2000@users.noreply.github.com>
2023-03-14 12:57:48 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,13 @@
+package com.my1;
+public class Student {
+    public static void main(String[] args) {
+        int a,b,s,m;
+        a=100;
+        b=200;
+        s=a+b;
+        m=a*b;
+        System.out.println("sum is:\n" +s);
+        System.out.println("multiplication is:\n" +m);
+
+    }
+}
